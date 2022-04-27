2d6e1538abbfef2d64a9be11fe2c32a520b53e77
littlestork4@gmail.com <andrew.ji@cupix.com>
2022-04-27 03:40:05 +0000

    Add 6 novel class

--- tools/model_surgery.py
+++ tools/model_surgery.py
@@ -83,13 +83,13 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     if args.dataset == 'coco':
-        NOVEL_CLASSES = [1, 2, 3, 4, 5, 6, 7, 9, 16, 17, 18, 19, 20, 21, 44, 62, 63, 64, 67, 72]
+        NOVEL_CLASSES = [1, 2, 3, 4, 5, 6, 7, 9, 16, 17, 18, 19, 20, 21, 44, 62, 63, 64, 67, 72,91,92,93,94,95,96]
         BASE_CLASSES = [8, 10, 11, 13, 14, 15, 22, 23, 24, 25, 27, 28, 31, 32, 33, 34, 35, 36, 37, 38,
                         39, 40, 41, 42, 43, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
                         61, 65, 70, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 84, 85, 86, 87, 88, 89, 90]
         ALL_CLASSES = sorted(BASE_CLASSES + NOVEL_CLASSES)
         IDMAP = {v: i for i, v in enumerate(ALL_CLASSES)}
-        TAR_SIZE = 80
+        TAR_SIZE = 86
     elif args.dataset == 'voc':
         TAR_SIZE = 20
     else:
